8ff12cc5fda19fd08bd7be753b8df11dd7052135
Abram17 <abram.albert.aa@gmail.com>
2023-08-10 10:12:02 +0000

    advanced 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main -Entry point
+ *
+ *Description: print a qoute using write function.
+ *
+ *Return: Always 1 (not success)
+*/
+
+int main(void)
+{
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.c.59);
+	return (1);
+}
+
