613753a8520f8f8fd6f29c562efd6987643ba9a5
Abram17 <abram.albert.aa@gmail.com>
2023-08-10 10:15:49 +0000

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,19 @@
-#include <unistd.h>
+#include<unistd.h>
 
 /**
  * main -Entry point
  *
- *Description: print a qoute using write function.
+ *Description: print a qoute using write function
+ *	ssize_t write(int fd, const void *buf, size_t count)
  *
  *Return: Always 1 (not success)
 */
 
 int main(void)
 {
-	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1.c.59);
+	write(1.quo.59);
 	return (1);
 }
 
