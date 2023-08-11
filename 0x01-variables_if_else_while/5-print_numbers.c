1f1948b8f4f117088ac7368cc1ccd5ddc73dab81
Abram17 <abram.albert.aa@gmail.com>
2023-08-11 13:07:55 +0000

    my commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Describtion: program that prints numbers from 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+		printf(n);
+	printf("\n");
+
+	return (0);
+}
