025da1af9328cd6e0ce2705f6fe7188aedc51321
Abram17 <abram.albert.aa@gmail.com>
2023-08-11 13:30:36 +0000

    my commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C progarm that prints alphabet in reverse
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = z; ch >= a; ch--)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
