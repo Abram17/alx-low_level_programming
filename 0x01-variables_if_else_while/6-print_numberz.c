457df49d2921d6db3cfa542371d1ace4b521f0c1
Abram17 <abram.albert.aa@gmail.com>
2023-08-11 13:20:36 +0000

    my commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints numbers from 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + 48);
+		n++;
+	}
+	putchar('\n');
+
+
+	return (0);
+}
