17b2be5dc7f81771c71ee1820df86314d182dbf7
Abram17 <abram.albert.aa@gmail.com>
2023-08-15 23:36:00 +0000

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num <=9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
 		for (mult = 1; mult <= 9; mult++)
@@ -18,10 +18,11 @@ void times_table(void)
 
 			prod = num * mult;
 			if (prod <= 9)
-				putchar(' ');
+				_putchar(' ');
 			else
-				_putchar((prod /10) + 48);
-			_putchar((prod %10) + 48);
+				_putchar((prod / 10) + 48);
+
+			_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
