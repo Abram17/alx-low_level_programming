b6b864aac41e913647520fa81652bad67217c89a
Abram17 <abram.albert.aa@gmail.com>
2023-08-16 01:01:44 +0000

    my commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -45,10 +45,10 @@ int main(void)
 			ini--;
 		}
 		printf("%lu", f1);
-		sum = (f1 + f2) % max;
+		sum = (f1 + f2) % mx;
 		sumo = flo + f2o + (f1 + f2) / mx;
-		f1 =f2;
-		flo = f20;
+		f1 = f2;
+		flo = f2o;
 		f2 = sum;
 		f2o = sumo;
 		if (count != 98)
