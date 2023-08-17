bfc3b3690f51adb6ae19c45623f4b9ac04216859
Abram17 <abram.albert.aa@gmail.com>
2023-08-17 23:05:46 +0000

    m

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,8 +3,6 @@
 
 /**
  * print_numbers - print 0-9
- *
- * Return: Always 0 (success)
  */
 
 void print_numbers(void)
@@ -14,6 +12,4 @@ void print_numbers(void)
 	for (num = 0; num <= 9; num++)
 		_putchar(num + 48);
 	_putchar('\n');
-
-	return (0);
 }
