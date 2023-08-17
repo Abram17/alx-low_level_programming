5cdcb35cd741e207855b959357c0e238218e8724
Abram17 <abram.albert.aa@gmail.com>
2023-08-17 23:53:12 +0000

    m

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,7 +19,7 @@ void print_diagonal(int n)
 		{
 			for (space = 1; space <= pos; space++)
 				_putchar(' ');
-			_putchar('\\');
+			_putchar('92');
 			if (pos == (n - 1)
 					continue;
 			_putchar('\n');
