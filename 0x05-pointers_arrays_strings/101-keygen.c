9fe4239515ee01d8ce4224519cb5b0e663fc6745
Abram17 <abram.albert.aa@gmail.com>
2023-08-21 22:59:05 +0000

    mandatory

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Descrription: generates random valid passwords
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2272 - sum);
+
+	return (0);
+}
