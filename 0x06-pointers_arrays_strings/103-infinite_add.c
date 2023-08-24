0af61a807ad07d2f8969f0301e4fb2d1350f61f1
Abram17 <abram.albert.aa@gmail.com>
2023-08-24 12:40:46 +0300

    error fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -30,12 +30,15 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		sum = carry;
 
 		if (len1 >= 0)
-			sum += n1[len1--] - '0';
+			sum += n1[len1] - '0';
+			len1--;
 		if (len2 >= 0)
-			sum += n2[len2--] - '0';
+			sum += n2[len2] - '0';
+			len2--;
 
 		carry = sum / 10;
-		r[index++] = sum % 10 + '0';
+		r[index] = sum % 10 + '0';
+		index++;
 	}
 
 	r[index] = '\0';
