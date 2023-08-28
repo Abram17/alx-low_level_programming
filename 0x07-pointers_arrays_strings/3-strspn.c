2dcda8613fe66caaba22c66761ab1a55f8d336d2
Abram17 <abram.albert.aa@gmail.com>
2023-08-28 22:16:57 +0300

    fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,15 +17,15 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] == ',')
+		{
+			break;
+		}
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				count++;
-			}
-			if (s[i] == ',' && s[i] != accept[j])
-			{
-				break;
 			}
 		}
 	}
