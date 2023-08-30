7761fa26a65cc582c5fc1e15b4ea25adf46f6b92
Abram17 <abram.albert.aa@gmail.com>
2023-08-31 01:29:57 +0300

    fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,11 +10,11 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 1;
+	int len = 0;
 
-	if (*s != '\0')
+	if (*s > '\0')
 	{
-		len += _strlen_recursion(s + 1);
+		len += _strlen_recursion(s + 1) + 1;
 	}
 
 	return (len);
