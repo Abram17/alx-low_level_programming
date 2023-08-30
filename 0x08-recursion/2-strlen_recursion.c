532829f9dcc48c803ec9922d0d47b8d796fc0e9c
Abram17 <abram.albert.aa@gmail.com>
2023-08-31 01:12:57 +0300

    done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of a string
+ *
+ * @s: input string
+ *
+ * Return: length of string
+*/
+
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = 0;
+
+	if (*s != '\0')
+	{
+		_strlen_recursion(s + 1);
+		len++;
+	}
+}
