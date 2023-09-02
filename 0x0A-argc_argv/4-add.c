e65cba555c4479b389de6929f741db1f8d0a1aa6
Abram17 <abram.albert.aa@gmail.com>
2023-09-02 18:59:53 +0300

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,57 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
 /**
- * main - Entry point
- *
- * Description: program that adds positive numbers.
- * @argc: count of arguments
- * @argv: array of arguments
- *
- * Return: 0 (success)
- *         1 (fail)
-*/
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ */
 
 int main(int argc, char *argv[])
 {
-	int i, sum, temp, x;
+	int i, j, length, sum;
 	char *ptr;
 
-	sum = 0;
-	if (argc != 1)
+	if (argc < 2)
+		printf("0\n");
+	else
 	{
+		sum = 0;
 		for (i = 1; i < argc; i++)
 		{
 			ptr = argv[i];
-			x = isdigit(*ptr);
-			if (x == 0)
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else
-			{
-				continue;
-			}
+
+			sum += atoi(argv[i]);
 		}
-		for (i = 1; i < argc; i++)
-		{
-			temp = strtol(argv[i], NULL, 10);
-			if (temp >= 0)
-			{
-				sum += temp;
-			}
-			else
-			{
-				continue;
-			}
-		}
-		printf("%d\n", sum);
-	}
-	else
-	{
-		printf("%d\n", 0);
+
+	printf("%d\n", sum);
 	}
 	return (0);
 }
