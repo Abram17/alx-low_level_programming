d09744e65fa011ca0f144dd3177e294fb1c0438a
Abram17 <abram.albert.aa@gmail.com>
2023-09-04 13:51:09 +0300

    done

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,50 @@
 #include "main.h"
 
 /**
+ * isWhitespace - determines whitespaces
+ *
+ * @c: input character
+ *
+ * Return: type of white space
+*/
+
+int isWhitespace(char c)
+{
+	return c == ' ' || c == '\t' || c == '\n';
+}
+
+/**
+ * countWords - counts words
+ *
+ * @str: input string
+ *
+ * Return: number of words
+*/
+
+int countWords(char* str)
+{
+	int count = 0;
+	int isWord = 0;
+
+	while (*str)
+	{
+		if (isWhitespace(*str))
+		{
+			isWord = 0;
+		}
+		else if (!isWord)
+		{
+			isWord = 1;
+			count++;
+		}
+
+		str++;
+	}
+
+	return count;
+}
+
+/**
  * strtow - splits a string
  *
  * @str: input string
@@ -9,26 +53,81 @@
  *         NULL if failed
 */
 
-char **strtow(char *str)
+char** strtow(char* str)
 {
-	int i;
-	char *ptr;
-
-	if (str == NULL || str == "")
-		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	ptr = (char *)malloc((i + 1) * sizeof(char));
-	if (ptr == NULL)
-		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
+	if (str == NULL || *str == '\0')
 	{
-		if (str[i] == " ")
-			ptr[i] = '\n';
-		else
-			ptr[i] = str[i];
+		return NULL;
 	}
-	return (ptr);
+
+	int numWords = countWords(str);
+	char** words = (char**)malloc((numWords + 1) * sizeof(char*));
+	if (words == NULL)
+	{
+		return NULL;
+	}
+
+	int wordIndex = 0;
+	int wordStart = 0;
+	int wordLength = 0;
+	int isWord = 0;
+
+	while (*str)
+	{
+		if (isWhitespace(*str))
+		{
+			if (isWord)
+			{
+				words[wordIndex] = (char*)malloc((wordLength + 1) * sizeof(char));
+				if (words[wordIndex] == NULL)
+				{
+					for (int i = 0; i < wordIndex; i++)
+					{
+						free(words[i]);
+					}
+					free(words);
+					return NULL;
+				}
+
+				strncpy(words[wordIndex], str - wordLength, wordLength);
+				words[wordIndex][wordLength] = '\0';
+				wordIndex++;
+				wordLength = 0;
+				isWord = 0;
+			}
+		} else if (!isWord)
+		{
+			isWord = 1;
+			wordStart = str - 1 - wordLength;
+			wordLength = 1;
+		}
+		else
+		{
+			wordLength++;
+		}
+
+		str++;
+	}
+
+	if (isWord)
+	{
+		words[wordIndex] = (char*)malloc((wordLength + 1) * sizeof(char));
+		if (words[wordIndex] == NULL)
+		{
+			for (int i = 0; i < wordIndex; i++)
+			{
+				free(words[i]);
+			}
+			free(words);
+			return NULL;
+		}
+
+		strncpy(words[wordIndex], str - wordLength, wordLength);
+		words[wordIndex][wordLength] = '\0';
+		wordIndex++;
+	}
+
+	words[wordIndex] = NULL;
+
+	return words;
 }
