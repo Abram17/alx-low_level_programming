d8d3cf99458f62c5719abcd4cbd6049008f354ee
Abram17 <abram.albert.aa@gmail.com>
2023-09-04 13:40:09 +0300

    fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,15 +11,15 @@
 
 char **strtow(char *str)
 {
-	int i, j;
-	char *ptr;
+	int i;
+	char **ptr;
 
 	if (str == NULL || str == "")
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
 		;
-	ptr = (char *)malloc((i + 1) * sizeof(char));
+	*ptr = (char *)malloc((i + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 
@@ -30,5 +30,5 @@ char **strtow(char *str)
 		else
 			ptr[i] = str[i];
 	}
-	return (ptr);
+	return (*ptr);
 }
