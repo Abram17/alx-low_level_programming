8fa8bdefcb47530d88bd2f7f8bba9c5a23e9a266
Abram17 <abram.albert.aa@gmail.com>
2023-09-04 13:37:40 +0300

    try1

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * strtow - splits a string
+ *
+ * @str: input string
+ *
+ * Return: pointer to new string
+ *         NULL if failed
+*/
+
+char **strtow(char *str)
+{
+	int i, j;
+	char *ptr;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	ptr = (char *)malloc((i + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == " ")
+			ptr[i] = '\n';
+		else
+			ptr[i] = str[i];
+	}
+	return (ptr);
+}
