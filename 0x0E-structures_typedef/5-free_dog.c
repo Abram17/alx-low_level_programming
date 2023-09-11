e57f4978cd2897f5d34755eaa6de4a39003dd7b0
Abram17 <abram.albert.aa@gmail.com>
2023-09-12 01:25:26 +0300

    done

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * free_dog - frees gogs
+  *
+  * @d: dog to free
+  *
+  * Return: void
+  */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
